5b197f7e8e6dede7a82660b2b5a92c95011d96c1
Luis Hernández Casado <47353055+lhernandezc96@users.noreply.github.com>
2024-01-29 13:59:30 +0100

    update

---
+++ 2_Analytics/14-Streamlit/main_parte1.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import json
+from streamlit_lottie import st_lottie
+
+st.set_page_config(page_title = "SUPERSTORE", page_icon=":chart", layout = "centered")
+st.title("Grupo SuperStore")
+st.subheader("Introducción")
